6aee9e36bd38825b0de29723bd469c4a1f516965
iamgavrysh <wokster110596@gmail.com>
2021-03-23 20:01:06 +0200

    changed ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,7 +103,7 @@ resource "aws_security_group" "bastion_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["62.122.202.215/32"]
+    cidr_blocks = ["x.x.x.x/32"]
   }
   egress {
     from_port   = 0
@@ -130,7 +130,7 @@ resource "aws_security_group" "allow_ssh_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["62.122.202.215/32"]
+    cidr_blocks = ["x.x.x.x/32"]
   }
   egress {
     from_port   = 0
